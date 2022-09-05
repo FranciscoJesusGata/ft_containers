6362da15220851a2ac9829a34f4d706abd51044e
Francisco jesus Gata valdes <fgata-va@student.42madrid.com>
2022-09-05 21:46:42 +0200

    Deleted c++11 functions

--- map.hpp
+++ map.hpp
@@ -6,7 +6,7 @@
 /*   By: fgata-va <fgata-va@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 22:13:17 by fgata-va          #+#    #+#             */
-/*   Updated: 2022/09/05 21:34:59 by fgata-va         ###   ########.fr       */
+/*   Updated: 2022/09/05 21:45:05 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,31 +87,12 @@ template <class Key, class T, class Compare = std::less<Key>, class Alloc = std:
 				return (iterator(NULL, &_tree));
 			}
 
-			const_iterator			cbegin() const {
-				node_type	*node = _tree.root;
-				while (node->left)
-					node = node->left;
-				return (const_iterator(node, &_tree));
-			}
-
-			const_iterator 			cend() const {
-				return (const_iterator(NULL, &_tree));
-			}
-
 			reverse_iterator		rbegin() {
 				return (reverse_iterator(end()));
 			}
 
-			const_reverse_iterator	crbegin() const {
-				return (const_reverse_iterator(cend()));
-			}
-
 			reverse_iterator		rend() {
 				return (reverse_iterator(begin()));
-			}
-
-			const_reverse_iterator	crend() {
-				return (const_reverse_iterator(cbegin()));
 			}
 
 			bool					empty() const {
@@ -128,22 +109,6 @@ template <class Key, class T, class Compare = std::less<Key>, class Alloc = std:
 
 			mapped_type				&operator[](const key_type& k) {
 				return ((*((this->insert(ft::make_pair(k,mapped_type()))).first)).second);
-			}
-			
-			mapped_type				&at(const key_type& k) {
-				node_type	*found = _tree.search(ft::make_pair(k,mapped_type()));
-
-				if (!found)
-					throw std::out_of_range("map::at:  key not found");
-				return (found->item.second);
-			}
-
-			const mapped_type&		at(const key_type& k) const {
-				node_type	*found = _tree.search(k);
-
-				if (!found)
-					throw std::out_of_range("map::at:  key not found");
-				return (found->item.second);
 			}
 
 			ft::pair<iterator,bool>	insert(const value_type& val)
