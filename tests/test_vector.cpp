4a61857d7661b9a33eb4cab9408dc5cbeca18dfc
Francisco Jesus Gata valdes <fgata-va@student.42madrid.com>
2023-01-08 22:15:11 +0100

    Dummy class added for testing, vector tests almost finished

--- tests/test_vector.cpp
+++ tests/test_vector.cpp
@@ -6,7 +6,7 @@
 /*   By: fgata-va <fgata-va@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 17:44:15 by fgata-va          #+#    #+#             */
-/*   Updated: 2022/09/06 18:19:18 by fgata-va         ###   ########.fr       */
+/*   Updated: 2023/01/08 22:14:36 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -126,6 +126,7 @@ void	at_test(ft::vector<T> const &v, size_t position) {
 
 template <class T>
 void	print_vector(ft::vector<T> &v) {
+	std::cout << "Size:" << v.size() << " ";
 	std::cout << "[ ";
 	for (typename ft::vector<T>::iterator it = v.begin(), end = v.end(); it != end; it++) {
 		std::cout << *it;
@@ -137,6 +138,7 @@ void	print_vector(ft::vector<T> &v) {
 
 template <class T>
 void	rev_print_vector(ft::vector<T> &v) {
+	std::cout << "Size:" << v.size() << " ";
 	std::cout << "[ ";
 	for (typename ft::vector<T>::reverse_iterator it = v.rbegin(), end = v.rend(); it != end; it++) {
 		std::cout << *it;
@@ -148,36 +150,51 @@ void	rev_print_vector(ft::vector<T> &v) {
 
 template <class T>
 void	print_vector_ln(ft::vector<T> &v) {
+	std::cout << "Size:" << v.size() << std::endl;
 	for (typename ft::vector<T>::iterator it = v.begin(), end = v.end(); it != end; it++)
-		std::cout << *it << std::endl;
+		std::cout << "| " << *it << std::endl;
 	std::cout << std::endl;
 }
 
 template <class T>
 void	rev_print_vector_ln(ft::vector<T> &v) {
+	std::cout << "Size:" << v.size() << std::endl;
 	for (typename ft::vector<T>::reverse_iterator it = v.rbegin(), end = v.rend(); it != end; it++)
-		std::cout << *it << std::endl;
+		std::cout << "| " << *it << std::endl;
 	std::cout << std::endl;
 }
 
+template <typename It1, typename It2>
+void	iterator_rel_op_tests(It1 const &lhs, It2 const &rhs)
+{
+	std::cout << (lhs == rhs) << std::endl;
+	std::cout << (lhs != rhs) << std::endl;
+	std::cout << (lhs <= rhs) << std::endl;
+	std::cout << (lhs < rhs) << std::endl;
+	std::cout << (lhs >= rhs) << std::endl;
+	std::cout << (lhs > rhs) << std::endl;
+}
+
 void	test_vector(void) {
-	ft::vector<int>	v;
 	std::cout << std::endl << "Capacity functions tests:" << std::endl;
-	std::cout << "\tMax size: " << v.max_size() << std::endl;
-	std::cout << "\tResize: ";
-	resize_test(v, 2, v.capacity());
-	resize_test(v, 10, 10, 42);
-	resize_test(v, 10, v.capacity(), 0);
-	resize_test(v, 0, v.capacity());
-	resize_test(v, -10, v.capacity());
-	resize_test(v, v.max_size(), v.max_size());
-	std::cout << std::endl << "\tReserve: ";
-	reserve_test(v, 0);
-	reserve_test(v, 10);
-	reserve_test(v, 20);
-	reserve_test(v, 42);
-	reserve_test(v, v.max_size());
-	reserve_test(v, -1);
+	{
+		ft::vector<int>	v;
+		std::cout << "\tMax size: " << v.max_size() << std::endl;
+		std::cout << "\tResize: ";
+		resize_test(v, 2, v.capacity());
+		resize_test(v, 10, 10, 42);
+		resize_test(v, 10, v.capacity(), 0);
+		resize_test(v, 0, v.capacity());
+		resize_test(v, -10, v.capacity());
+		resize_test(v, v.max_size(), v.max_size());
+		std::cout << std::endl << "\tReserve: ";
+		reserve_test(v, 0);
+		reserve_test(v, 10);
+		reserve_test(v, 20);
+		reserve_test(v, 42);
+		reserve_test(v, v.max_size());
+		reserve_test(v, -1);
+	}
 	std::cout << std::endl << std::endl << "Constructors tests:" << std::endl;
 	std::cout << "\tFill constructor:";
 	ft::vector<int> stuff(5, 100);
@@ -207,10 +224,10 @@ void	test_vector(void) {
 	empty = stuff;
 	vector_cmp<int>(empty, stuff);
 	ft::vector<int> ft(42,42);
-	std::cout << "\t\tBefore: ";
+	std::cout << "\t\tBefore: " << std::endl;
 	print_vector(ft);
 	ft = stuff;
-	std::cout << "\t\tAfter: ";
+	std::cout << "\t\tAfter: " << std::endl;
 	print_vector(ft);
 
 	std::cout << "Relational operators tests: " << std::endl;
@@ -224,12 +241,17 @@ void	test_vector(void) {
 	test_cmp(ft, stuff);
 	std::cout << std::endl << std::endl << "Element access functions tests:" << std::endl;
 	std::cout << "\tAt: ";
-	at_test(empty, 0);
-	at_test(empty, 1);
-	at_test(range, 0);
-	at_test(range, range.size() - 1);
-	at_test(range, range.size());
-	at_test(range, -1);
+	{
+		ft::vector<int> const v_const(empty);
+		at_test(empty, 0);
+		at_test(empty, 1);
+		at_test(range, 0);
+		for (size_t i = 0; i < v_const.size() ; i++)
+			at_test(v_const, i);
+		at_test(range, range.size() - 1);
+		at_test(range, range.size());
+		at_test(range, -1);
+	}
 	std::cout << std::endl << "\tBack and Front: ";
 	std::cout << (stuff[0] == stuff.front() ? "OK! " : "KO Front function doesn't return first element ");
 	std::cout << (stuff[stuff.size() - 1] == stuff.back() ? "OK! " : "KO Back function doesn't return last element");
@@ -239,11 +261,48 @@ void	test_vector(void) {
 
 	std::cout << std::endl << std::endl << "Modifiers functions tests:" << std::endl;
 	std::cout << "\tAssign: " << std::endl;
-	std::cout << "\t\tBefore assign: ";
-	print_vector(empty);
-	empty.assign(10, 21);
-	std::cout << "\t\tAfter assign: ";
-	print_vector(empty);
+	{
+		ft::vector<int>	first(10);
+		ft::vector<int> second(5);
+		ft::vector<int> third;
+		ft::vector<int> forth;
+		for (size_t i = 0; i < first.size() ; i++)
+			first[i] = first.size() - i - 1;
+		for (size_t i = 0; i < second.size() ; i++)
+			second[i] = (i + 1) * 5;
+		std::cout << "\t\tBefore assign: " << std::endl;
+		print_vector(first);
+		print_vector(second);
+		third.assign(first.begin(), first.end());
+		forth.assign(third.begin(), third.end() - 5);
+		first.assign(5, 21);
+		second.assign(2, 42);
+		std::cout << "\t\tAfter assign: " << std::endl;
+		print_vector(first);
+		print_vector(second);
+		print_vector(third);
+		print_vector(forth);
+	}
+	std::cout << std::endl << "\tPush back and pop back: " << std::endl;
+	{
+		ft::vector<std::string> toSmart;
+		ft::vector<std::string> tooProgramCpp(10);
+
+		for (size_t i = 0 ; i < 10 ; i++)
+			tooProgramCpp[i] = std::string(i + 1, i + 65);
+		print_vector(tooProgramCpp);
+		for (int i = tooProgramCpp.size() - 1 ; i >= 0 ; i--)
+			toSmart.push_back(tooProgramCpp[i]);
+		print_vector(toSmart);
+		toSmart.push_back("Sigma male based red pilled programer (please help)");
+		tooProgramCpp.push_back("I rly am to smart too program in c++ bruh");
+		print_vector(toSmart);
+		print_vector(tooProgramCpp);
+		toSmart.pop_back();
+		tooProgramCpp.pop_back();
+		print_vector(toSmart);
+		print_vector(tooProgramCpp);
+	}
 	std::cout << std::endl << "\tSwap: " << std::endl;
 	std::cout << "\t\tBefore swap: " << std::endl;
 	std::cout << "\tV1: ";
@@ -267,54 +326,172 @@ void	test_vector(void) {
 	print_vector(empty);
 	std::cout << "\t\tempty: " << std::boolalpha << empty.empty() << std::noboolalpha << std::endl;
 	std::cout << "\tInsert:" << std::endl;
-	ft::vector<int>	equisde(20);
-	ft::vector<int> lol;
-	for (size_t i = 0; i < equisde.size() ; i++)
-		equisde[i] = (equisde.size() - i) * 3;
-	lol.insert(lol.end(), 42);
-	lol.insert(lol.begin(), 5, 21);
-	print_vector(lol);
-	lol.insert(lol.end() - 1, 4);
-	print_vector(lol);
-	lol.insert(lol.end(), 2, 2);
-	print_vector(lol);
-	lol.insert(lol.begin() + 2, equisde.begin(), equisde.end());
-	equisde.clear();
-	print_vector(lol);
-	for (int i = 0; i < 10 ; ++i)
-		equisde.insert(equisde.end(), i);
-	equisde.insert(equisde.begin() + 3, 2, 100);
-	print_vector(equisde);
-	ft::vector<std::string> abc;
-	for (int i = 0; i < 100 ; ++i)
-		abc.insert(abc.end(), std::string((i % 3) + 1, (i % 26) + 'a'));
-	print_vector(abc);
-	abc.insert(abc.begin(), abc.begin(), abc.begin() + 5);
-	print_vector(abc);
-	std::cout << "\tErase:" << std::endl;
-	abc.erase(abc.begin() + 2);
-	print_vector(abc);
-	abc.erase(abc.begin());
-	print_vector(abc);
-	abc.erase(abc.end() - 1);
-	print_vector(abc);
-	abc.erase(abc.begin(), abc.begin() + 4);
-	print_vector(abc);
-	abc.erase(abc.begin(), abc.begin() + 4);
-	print_vector(abc);
-	abc.erase(abc.end() - 4, abc.end() - 1);
-	print_vector(abc);
-	abc.push_back("abc");
-	abc.push_back("def");
-	abc.push_back("ghi");
-	print_vector(abc);
-	abc.erase(abc.end() - 4, abc.end() - 1);
-	print_vector(abc);
-	abc.push_back("LET'S");
-	abc.push_back("ALL");
-	abc.push_back("LOVE");
-	abc.push_back("LAIN");
-	print_vector(abc);
-	abc.erase(abc.begin(), abc.end());
-	print_vector(abc);
+	{
+		ft::vector<int>	equisde(20);
+		ft::vector<int> lol;
+		for (size_t i = 0; i < equisde.size() ; i++)
+			equisde[i] = (equisde.size() - i) * 3;
+		lol.insert(lol.end(), 42);
+		lol.insert(lol.begin(), 5, 21);
+		print_vector_ln(lol);
+		lol.insert(lol.end() - 1, 4);
+		print_vector_ln(lol);
+		lol.insert(lol.end(), 2, 2);
+		print_vector_ln(lol);
+		lol.insert(lol.begin() + 2, equisde.begin(), equisde.end());
+		equisde.clear();
+		print_vector_ln(lol);
+		for (int i = 0; i < 10 ; ++i)
+			equisde.insert(equisde.end(), i);
+		equisde.insert(equisde.begin() + 3, 2, 100);
+		print_vector_ln(equisde);
+	}
+	{
+		ft::vector<std::string> abc;
+		for (int i = 0; i < 100 ; ++i)
+			abc.insert(abc.end(), std::string((i % 3) + 1, (i % 26) + 'a'));
+		print_vector_ln(abc);
+		abc.insert(abc.begin(), abc.begin(), abc.begin() + 5);
+		print_vector_ln(abc);
+		std::cout << "\tErase:" << std::endl;
+		abc.erase(abc.begin() + 2);
+		print_vector_ln(abc);
+		abc.erase(abc.begin());
+		print_vector_ln(abc);
+		abc.erase(abc.end() - 1);
+		print_vector_ln(abc);
+		abc.erase(abc.begin(), abc.begin() + 4);
+		print_vector_ln(abc);
+		abc.erase(abc.begin(), abc.begin() + 4);
+		print_vector_ln(abc);
+		abc.erase(abc.end() - 4, abc.end() - 1);
+		print_vector_ln(abc);
+		abc.push_back("abc");
+		abc.push_back("def");
+		abc.push_back("ghi");
+		print_vector_ln(abc);
+		abc.erase(abc.end() - 4, abc.end() - 1);
+		print_vector_ln(abc);
+		abc.push_back("LET'S");
+		abc.push_back("ALL");
+		abc.push_back("LOVE");
+		abc.push_back("LAIN");
+		print_vector_ln(abc);
+		abc.erase(abc.end() - 4, abc.end());
+		print_vector_ln(abc);
+		abc.erase(abc.begin(), abc.end());
+		print_vector_ln(abc);
+	}
+	std::cout << std::endl << std::endl << "Vector iterator tests:" << std::endl;
+	{
+		ft::vector<std::string> wow;
+		for (int i = 0 ; i < 10 ; i++)
+			wow.push_back(std::string((i % 3) + 1, i + 'a'));
+		ft::vector<std::string>::iterator it = wow.begin();
+		ft::vector<std::string>::iterator it2;
+		ft::vector<std::string>::const_iterator c_it;
+		
+		std::cout << "\tPre and post incremend and decrement: " << std::endl;
+		std::cout << "\t\tPreincrement" << std::endl;
+		it2 = ++it;
+		std::cout << "\t\t" << *it << " " << *it2 << std::endl;
+		std::cout << "\t\tPosincrement" << std::endl;
+		it2 = it++;
+		std::cout  << "\t\t" << *it << " " << *it2 << std::endl;
+		std::cout << "\t\tPredecrement" << std::endl;
+		it2 = --it;
+		std::cout  << "\t\t" << *it << " " << *it2 << std::endl;
+		std::cout << "\t\tPosdecrement" << std::endl;
+		it2 = it--;
+		std::cout  << "\t\t" << *it << " " << *it2 << std::endl;
+		
+		std::cout << "\tIncrement, decrement and assing operators:" << std::endl;
+		it = it + 4;
+		it = 2 + it;
+		it  = it - 4;
+		std::cout << "\t\t"  << *it << std::endl;
+		std::cout << "\t\t" << *(it += 4) << std::endl;
+		std::cout << "\t\t" << *(it += 2) << std::endl;
+		*(it -= 2) = "🌯";
+		*(it += 2) = "👀";	
+		it -= 4;
+		c_it = it;
+		std::cout << "\t\t" << *(c_it += 4) << std::endl;
+		std::cout << "\t\t" << *(c_it -= 2) << std::endl;
+		std::cout << std::boolalpha;
+		std::cout << "\t\titerator == const iterator " << (c_it == it) << std::endl;
+		std::cout << std::noboolalpha;
+		std::cout << "\t\titerator - const iterator " << (it - c_it) << std::endl;
+		std::cout << "\t\tconst iterator - iterator " << (c_it - it) << std::endl;
+		std::cout << "\t\tconst iterator + 3 - iterator " << (c_it + 3 - it) << std::endl;
+		std::cout << "\t\titerator + 2 - const iterator " << (it + 2 - c_it) << std::endl;
+	}
+	{
+		std::cout << "\tDereference operators:" << std::endl;
+		ft::vector<Dummy<int> > dummyVector(10);
+		ft::vector<Dummy<int> >::iterator it = dummyVector.begin();
+		ft::vector<Dummy<int> >::iterator middle;
+		ft::vector<Dummy<int> >::iterator end = dummyVector.end();;
+		ft::vector<Dummy<int> >::const_iterator c_it;
+		ft::vector<Dummy<int> >::const_iterator c_middle;
+		ft::vector<Dummy<int> >::const_iterator c_end;
+
+		for	(int i = 1 ; it != end ; it++) {
+			*it = (i + 1) * 2;
+			i++;
+		}
+	
+		it = dummyVector.begin();	
+		c_it = it;
+
+		std::cout << *(it++) << std::endl;
+		std::cout << *(++it) << std::endl;
+		std::cout << *(--it) << std::endl;
+		std::cout << *(it--) << std::endl;
+
+		std::cout << *(c_it++) << std::endl;
+		std::cout << *(++c_it) << std::endl;
+		std::cout << *(--c_it) << std::endl;
+		std::cout << *(c_it--) << std::endl;
+	
+		std::cout << *it++ << std::endl;
+		std::cout << *++it << std::endl;
+		std::cout << *--it << std::endl;
+		std::cout << *it-- << std::endl;
+
+		std::cout << *c_it++ << std::endl;
+		std::cout << *++c_it << std::endl;
+		std::cout << *--c_it << std::endl;
+		std::cout << *c_it-- << std::endl;
+
+		it->dummyMethod();
+		c_it->dummyMethod();
+		(*it).dummyMethod();
+		(*c_it).dummyMethod();
+
+		std::cout << "\tRelational operators tests:" << std::endl;
+		std::cout << std::boolalpha;
+		it = dummyVector.begin();
+		c_it = dummyVector.begin();
+		middle = it + 5;
+		c_middle = middle;		
+		end = dummyVector.end();
+		c_end = end;
+		std::cout << ((it + 5 == c_it + 5) && (c_it + 5 == middle)) << std::endl;
+		iterator_rel_op_tests(it, middle);
+		iterator_rel_op_tests(it, end);
+		iterator_rel_op_tests(c_it, c_middle);
+		iterator_rel_op_tests(it, c_end);
+		iterator_rel_op_tests(c_it, it);
+		iterator_rel_op_tests(c_middle, middle);
+		iterator_rel_op_tests(c_end, end);
+		iterator_rel_op_tests(c_it, middle);
+		iterator_rel_op_tests(c_it, end);
+		iterator_rel_op_tests(it, c_middle);
+		iterator_rel_op_tests(it, c_end);
+	}
+	std::cout << std::endl << std::endl << "Vector reverse iterator tests:" << std::endl;
+	{
+		//TODO: Add the same tests for reverse iterator
+	}
 }
--- tests/tester.hpp
+++ tests/tester.hpp
@@ -6,7 +6,7 @@
 /*   By: fgata-va <fgata-va@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 17:41:06 by fgata-va          #+#    #+#             */
-/*   Updated: 2022/09/06 17:46:16 by fgata-va         ###   ########.fr       */
+/*   Updated: 2023/01/08 21:55:35 by fgata-va         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@
 # include <cstdlib>
 # include <ctime>
 # include <deque>
+# include <ostream>
 //Subject test stuff
 # define MAX_RAM 4294967296
 # define BUFFER_SIZE 4096
@@ -57,6 +58,61 @@ public:
 	iterator begin() { return this->c.begin(); }
 	iterator end() { return this->c.end(); }
 };
+
+template <typename T>
+class Dummy {
+	public:
+		typedef T	value_type;
+
+		Dummy(void): item() {}
+		Dummy(value_type v): item(v) {}
+		Dummy(Dummy const &src): item(src.item) {}
+		~Dummy() {}
+
+		Dummy	&operator=(Dummy const &src) {
+			if (src != *this)
+				item = src.item;
+			return (*this);
+		}
+
+		Dummy	&operator=(T const &src) {
+			item = src;
+			return (*this);
+		}
+
+		void	dummyMethod(void) {
+			std::cout << "This is a Dummy method, this instance have a item with a value of: " << item << std::endl;
+		}
+
+		void	dummyMethod(void) const {
+			std::cout << "This is a Dummy method, this instance is const and have a item with a value of: " << item << std::endl;
+		}
+
+		bool	operator==(Dummy const &rhs) const {
+			return (item == rhs.item);
+		}
+		
+		bool	operator!=(Dummy const &rhs) const {
+			return (!(*this == rhs));
+		}
+
+		T		&operator*(void) {
+			return (item);
+		}
+
+		T		get_item(void) const {
+			return (item);
+		}
+
+	private:
+		value_type	item;
+};
+
+template <typename T>
+std::ostream	&operator<<(std::ostream &o, const Dummy<T> &rhs) {
+	o << rhs.get_item(); 
+	return (o);
+}
 
 void	test_vector(void);
 void	test_map(void);
